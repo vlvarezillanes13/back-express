39402b8142088f5006bc9d30fcba9b3a33acd3a5
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2022-09-11 20:03:42 -0300

    registrar usuario con token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,46 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
+const bcrypt = require('bcryptjs');
+const { generarJWT } = require('../helpers/jwt');
 
+const crearUsuario = async (req, resp = response) => {
 
-const crearUsuario = (req, resp = response) => {
+    const { name, email, password } = req.body;
 
-    resp.json({
-        ok:true,
-        msg:"New"
-    })
+    try{
+        //Verificar el email
+        const usuario = await Usuario.findOne({ email});
+        if( usuario ){
+            return resp.status(400).json({
+                ok:false,
+                msg:"El usuario ya existe"
+            })
+        }
+        //Crear usuario con el modelo
+        const dbUser = new Usuario( req.body );
+        //Hashear la contraseña
+        const salt = bcrypt.genSaltSync( 10 );
+        dbUser.password = bcrypt.hashSync( password, salt);
+        //Generar el JWT
+        const token = await generarJWT( dbUser.id, dbUser.name );
+
+        //Crear usuario BD
+        await dbUser.save();
+
+        //Generar respuesta exitosa
+        return resp.status(201).json({
+            ok:true,
+            uid: dbUser.id,
+            name,
+            token
+        })
+
+    }catch(errors){
+        return resp.status(500).json({
+            ok:false,
+            msg:"Contactarse con el administrador"
+        });
+    }
 };
 
 const loginUsuario = (req, resp = response) => {
