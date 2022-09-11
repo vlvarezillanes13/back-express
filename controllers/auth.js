785a7081eec7dc4529732b19baf19d5cfd113dc8
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2022-09-11 18:55:03 -0300

    rutas, controllers, middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,4 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
 
 const crearUsuario = (req, resp = response) => {
@@ -12,13 +11,6 @@ const crearUsuario = (req, resp = response) => {
 
 const loginUsuario = (req, resp = response) => {
 
-    const errors = validationResult( req );
-    if( !errors.isEmpty() ){
-        return resp.status(400).json({
-            ok:false,
-            errors: errors.mapped()
-        })
-    }
     resp.json({
         ok:true,
         msg:"Login"
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,23 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validaCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 //Crear Usuario
-router.post( '/new', crearUsuario );
+router.post( '/new',[
+    check('name', 'El nombre es obligaotrio').not().isEmpty(),
+    check('email', 'El email es obligaotrio').isEmail(),
+    check('password', 'La contraseña es obligaotria').isLength({ min: 6}),
+    validaCampos
+], crearUsuario );
 
 //Login de usuario
 router.post( '/', [
     check('email', 'El email es obligaotrio').isEmail(),
-    check('password', 'La contraseña es obligaotria').isLength({ min: 6})
+    check('password', 'La contraseña es obligaotria').isLength({ min: 6}),
+    validaCampos
 ],loginUsuario );
 
 //Validar y Revalidar token
